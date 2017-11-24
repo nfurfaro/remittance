314fd62d1988773f25823eba530f463da0607823
n_44o <nfurfaro33@gmail.com>
2017-11-24 16:13:53 -0700

    Refactor tests

--- contracts/Remittance.sol
+++ contracts/Remittance.sol
@@ -4,122 +4,61 @@ import "./Freezable.sol";
 
 contract Remittance is Freezable {
 
-    // address public Carol;
-    uint public fundsForBob;
-    bytes32 public hashedPasswords;
-    bool public fundsUnlocked;
-    
-    event LogRemittance(address depositor, uint amount);
-    event LogClaim(address claimant, uint funds);
-
-// needs hash match to access a box. this coul be a password, pin or combo
-// hash generated from keccak256(address recipient, uint amount, uint8 pinNumber)
-
-// recipient needs the right hash to enter, then the right address to access the funds
-
     struct LockBox {
-        address recipient;
         uint fundsWaiting;
-        bytes32 previousKey;
+        bool invalidKey;
     }
 
     mapping(bytes32 => LockBox) public LockBoxCluster;
     address[] public recipientAddresses;
 
-    function Remittance() {
-        // Carol = _Carol;
-    }
+    event LogRemittance(address depositor, uint amount);
+    event LogClaim(address claimant, uint funds, bool _invalidKey);
+   
+    function Remittance() {}
 
-    // 1.)client-side function!
+
+
+// for testing use!!
     // function passwordHasher(string _password)
     //     public
     //     pure
     //     returns(bytes32 hashedPassword)
     // {    
     //     return keccak256(_password);
-    // }
-    
-    // 2.)passwords hashed client-side by passwordHasher()
-    // function setPasswords(bytes32 _hashedPassword1, bytes32 _hashedPassword2)
-    //     public
-    //     freezeRay
-    //     onlyOwner
-    //     returns (bool success)
-    // {
-    //     hashedPasswords = keccak256(_hashedPassword1, _hashedPassword2);
-    //     return true;
-    // }    
+    // }   
 
-    // key is the hash of 2 passwords, calculated client-side.
-    function sendRemittance(bytes32 _Key, address _recipient)
+
+
+    // _password could be hashed, but I'm not sure that hashing them provides an extra layer of security, as the passwords are single-use only.(actually, the key is single use)
+    function sendRemittance(string _password, address _recipient)
         public
         onlyOwner
         payable
         freezeRay
         returns (bool success)
     {
-        LockBoxCluster[_Key].recipient = _recipient;
-        LockBoxCluster[_Key].fundsWaiting += msg.value;
-        recipientAddresses.push(msg.sender);
-        LogRemittance(msg.sender, msg.value); 
+        bytes32 key = keccak256(_password, _recipient);
+        require(LockBoxCluster[key].invalidKey == false); 
+        LockBoxCluster[key].fundsWaiting += msg.value;
+        recipientAddresses.push(_recipient);
+        LogRemittance(msg.sender, msg.value);
+        return true; 
     }
 
-    function claimRemittance(bytes32 _password)
+//this takes a _password, which is a concatenation of 2 passwords(Bob's & Carol's), done client-side.
+    function claimRemittance(string _password)
         public
         freezeRay
         returns (bool success)
     { 
-        // require(LockBoxCluster[_password].previousKey != _password);
-        // LockBoxCluster[_password].previousKey = _password;
-        require(msg.sender == LockBoxCluster[_password].recipient);
-        require(LockBoxCluster[_password].fundsWaiting != 0);
-        uint funds = LockBoxCluster[_password].fundsWaiting;
-        LockBoxCluster[_password].fundsWaiting = 0;
+        bytes32 key = keccak256(_password, msg.sender);
+        require(LockBoxCluster[key].fundsWaiting != 0);
+        LockBoxCluster[key].invalidKey = true;
+        uint funds = LockBoxCluster[key].fundsWaiting;
+        LockBoxCluster[key].fundsWaiting = 0;
         msg.sender.transfer(funds);
-        LogClaim(msg.sender, msg.value);
+        LogClaim(msg.sender, funds, LockBoxCluster[key].invalidKey);
+        return true;
     }
-
-    // function changeKeys
- 
-    // function deposit()
-    //     freezeRay
-    //     onlyOwner
-    //     public 
-    //     payable 
-    //     returns (bool success)
-    // {  
-    //     require(hashedPasswords != bytes32(0));
-    //     require(msg.value != 0);
-    //     fundsForBob += msg.value;
-    //     LogDeposit(msg.sender, msg.value);
-    //     return true;
-    // }
-    
-    // function validatePasswords(bytes32 _hashedPassword1, bytes32 _hashedPassword2)
-    //     public
-    //     freezeRay
-    //     returns (bool success)
-    // {
-    //     require(msg.sender == Carol);
-    //     require(keccak256(_hashedPassword1, _hashedPassword2) == hashedPasswords);
-    //     fundsUnlocked = true;
-    //     return true; 
-    // }
-
-    // function withdrawFunds()
-    //     public
-    //     freezeRay
-    //     returns (bool success)
-    // {
-    //     require(msg.sender == Carol);
-    //     require(fundsUnlocked == true);
-    //     require(fundsForBob != 0);
-    //     uint amount = fundsForBob; 
-    //     fundsForBob = 0;
-    //     fundsUnlocked = false;
-    //     LogWithdrawal(msg.sender, amount);
-    //     msg.sender.transfer(amount);
-    //     return true; 
-    // }    
-    
 }
--- test/remittance.js
+++ test/remittance.js
@@ -1,5 +1,7 @@
 const Remittance = artifacts.require("./Remittance.sol");
 const expectedExceptionPromise = require("../expected_exception_testRPC_and_geth.js");
+const abi = require('ethereumjs-abi');
+
 const Promise = require("bluebird")
 
 if (typeof web3.eth.getBlockPromise !== "function") {
@@ -24,91 +26,151 @@ contract('Remittance', accounts => {
         assert.strictEqual(_owner, owner, "Contract is not owned by owner");
     });
 
-    it("should return a correctly-hashed password", () => {
-        let password = "peanut-butter-sandwich";
-        return instance.passwordHasher(password, {from: owner})
-            .then(_hash => {
-                assert.strictEqual(_hash, "0xbf99d12625bf06ca9eb22dcd41ab8f3e00814126e293aed5c6365f5752512d36", "password wasn't hashed or returned correctly");
+    it("should allow the owner to send a remittance for Carol", () => {
+        let remittanceAmount = 60000;
+        let event = "LogRemittance";
+        let _event;
+        let loggedAmount;
+        let depositor;
+        let Part1 = "password-Bobs-Part";
+        let Part2 = "password-Carols-Part";
+        let password = Part1 + Part2;
+        // let key = abi.soliditySHA3([password, Carol.toString(10)]).toString('hex');
+        let key = web3.sha3(password, Carol);
+        console.log(key);
+        return instance.sendRemittance(password, Carol, {from: owner, value: remittanceAmount})
+            .then(txObj => {
+                _event = txObj.logs[0].event.toString(10);
+                depositor = txObj.logs[0].args.depositor;
+                loggedAmount = txObj.logs[0].args.amount.toString(10);
+                return instance.LockBoxCluster(key, {from: Carol})
             })
-    });
+            .then(_lockBox => {
+                console.log(_lockBox);
+                let fundsWaiting = _lockBox[0].toString(10);
+                let invalidKey = _lockBox[1];
+                (10);
+                assert.strictEqual(_event, event, "the LogRemittance event did not occur");
+                assert.strictEqual(loggedAmount, remittanceAmount.toString(10), "the LogRemittance event did not occur");
+                assert.strictEqual(depositor, owner, "the correct sender was not logged.");
+                assert.strictEqual(fundsWaiting, remittanceAmount, "Carol's LockBox.fundsWaiting state was not set correctly");
+                assert.strictEqual(invalidKey, false, "Carol's LockBox.usedKey state was not set correctly");
+            })
+    })
 
-    it("should allow the owner to set the passwords", () => {
-        let hashed1 = 0xbf99d12625bf06ca9eb22dcd41ab8f3e00814126e293aed5c6365f5752512d36
-        let hashed2 = 0x754b1823247e990c20230ca0d8806c29508cc9244c059c09a1869b56ab64dacd
-        return instance.setPasswords(hashed1, hashed2, {from:owner})
+    it("should allow Carol to claim her funds", () => {
+        let remittanceAmount = 60000;
+        let event = "LogRemittance";
+        let _event;
+        let loggedAmount;
+        let depositor;
+        let Part1 = "password-Bobs-Part";
+        let Part2 = "password-Carols-Part";
+        let password = Part1 + Part2;
+        return instance.sendRemittance(password, Carol, {from: owner, value: remittanceAmount})
             .then(() => {
-                return instance.hashedPasswords()
+                return instance.LockBoxCluster(password, {from: Carol})
             })
-            .then(_hash => {
-                assert.strictEqual(_hash.toString(10), "0x88dc9fe9b1acbfe7d31ded4b484a30e4233e51b3228d9eb9e3bbb801a5b8a482", " the passwords were not set correctly")
+            .then(_lockbox => {
+                console.log(_lockbox);
+                return instance.claimRemittance(password, {from: Carol}) 
+            })
+            .then(txObj => {
+                console.log(txObj.logs[0].args);
+                return instance.LockBoxCluster(password, {from: Carol})
+            })
+            .then(_lockbox => {
+                console.log(_lockbox);
             })
     })
 
-    it("should not allow anyone but the owner to set the passwords", () => {
-        let unauthorizedPerson = accounts[1];
-        let hashed1 = 0xbf99d12625bf06ca9eb22dcd41ab8f3e00814126e293aed5c6365f5752512d36
-        let hashed2 = 0x754b1823247e990c20230ca0d8806c29508cc9244c059c09a1869b56ab64dacd
-        expectedExceptionPromise(() => {
-            return instance.setPasswords(hashed1, hashed2, {from:unauthorizedPerson});
-        })
-    })
+    // it("should return a correctly-hashed password", () => {
+    //     let password = "peanut-butter-sandwich";
+    //     return instance.passwordHasher(password, {from: owner})
+    //         .then(_hash => {
+    //             assert.strictEqual(_hash, "0xbf99d12625bf06ca9eb22dcd41ab8f3e00814126e293aed5c6365f5752512d36", "password wasn't hashed or returned correctly");
+    //         })
+    // });
 
-    it("should add a new deposit to the contract balance", () => {
-        let hashed1 = 0xbf99d12625bf06ca9eb22dcd41ab8f3e00814126e293aed5c6365f5752512d36
-        let hashed2 = 0x754b1823247e990c20230ca0d8806c29508cc9244c059c09a1869b56ab64dacd
-        let deposit= 1000;
-        let expected = "1000";
-        let event = "LogDeposit";
-        let contractBalanceBefore;
-        let contractBalanceAfter;
-        return web3.eth.getBalancePromise(instance.address)
-            .then(_balance  => {
-                contractBalanceBefore = _balance;
-                return instance.setPasswords(hashed1, hashed2, {from:owner})
-            })
-            .then(() => {
-                return instance.deposit({from: owner, value: deposit})  
-            }).then(txObj => {
-                assert.strictEqual(txObj.logs[0].event, event, "a deposit was not logged");
-                return web3.eth.getBalancePromise(instance.address)
-            }).then(_balance  => {
-                contractBalanceAfter = _balance;
-                assert.strictEqual(_balance.toString(10), expected, "Splitter balance was not increased by 10 wei");
-            })
-    });
+    // it("should allow the owner to set the passwords", () => {
+    //     let hashed1 = 0xbf99d12625bf06ca9eb22dcd41ab8f3e00814126e293aed5c6365f5752512d36
+    //     let hashed2 = 0x754b1823247e990c20230ca0d8806c29508cc9244c059c09a1869b56ab64dacd
+    //     return instance.setPasswords(hashed1, hashed2, {from:owner})
+    //         .then(() => {
+    //             return instance.hashedPasswords()
+    //         })
+    //         .then(_hash => {
+    //             assert.strictEqual(_hash.toString(10), "0x88dc9fe9b1acbfe7d31ded4b484a30e4233e51b3228d9eb9e3bbb801a5b8a482", " the passwords were not set correctly")
+    //         })
+    // })
 
-    it("should allow Carol to unlock the funds by validating her passwords", async() => {
-        let hashed1 = 0xbf99d12625bf06ca9eb22dcd41ab8f3e00814126e293aed5c6365f5752512d36;
-        let hashed2 = 0x754b1823247e990c20230ca0d8806c29508cc9244c059c09a1869b56ab64dacd;
-        let result;
-        let deposit = 1000;
-        await instance.setPasswords(hashed1, hashed2, {from:owner});
-        await instance.deposit({from: owner, value: deposit});
-        await instance.validatePasswords(hashed1, hashed2, {from: Carol});
-        result = await instance.fundsUnlocked();    
-        assert.strictEqual(result, true, "Carol was not able to validate her passwords");
-    })
+    // it("should not allow anyone but the owner to set the passwords", () => {
+    //     let unauthorizedPerson = accounts[1];
+    //     let hashed1 = 0xbf99d12625bf06ca9eb22dcd41ab8f3e00814126e293aed5c6365f5752512d36
+    //     let hashed2 = 0x754b1823247e990c20230ca0d8806c29508cc9244c059c09a1869b56ab64dacd
+    //     expectedExceptionPromise(() => {
+    //         return instance.setPasswords(hashed1, hashed2, {from:unauthorizedPerson});
+    //     })
+    // })
 
-    it("should confirm that carol can withdraw her funds", async() => {
-        let startBalance;
-        let payout;
-        let gasPrice;
-        let gasUsed;
-        let txFee;
-        let endBalance;
-        let hashed1 = 0xbf99d12625bf06ca9eb22dcd41ab8f3e00814126e293aed5c6365f5752512d36;
-        let hashed2 = 0x754b1823247e990c20230ca0d8806c29508cc9244c059c09a1869b56ab64dacd;
-        let deposit = 1000;
-        startBalance = await web3.eth.getBalancePromise(Carol);
-        await instance.setPasswords(hashed1, hashed2, {from:owner});
-        await instance.deposit({from: owner, value: deposit});
-        await instance.validatePasswords(hashed1, hashed2, {from: Carol}); 
-        txObj = await instance.withdrawFunds({from: Carol});
-        gasUsed = txObj.receipt.gasUsed;
-        tx = await web3.eth.getTransactionPromise(txObj.tx);
-        gasPrice = tx.gasPrice;
-        txFee = gasPrice.times(gasUsed);
-        endBalance = await web3.eth.getBalancePromise(Carol)
-        assert.strictEqual(startBalance.plus(deposit).minus(txFee).toString(10), endBalance.toString(10), "Carol didn't get her ether")  
-    })
+    // it("should add a new deposit to the contract balance", () => {
+    //     let hashed1 = 0xbf99d12625bf06ca9eb22dcd41ab8f3e00814126e293aed5c6365f5752512d36
+    //     let hashed2 = 0x754b1823247e990c20230ca0d8806c29508cc9244c059c09a1869b56ab64dacd
+    //     let deposit= 1000;
+    //     let expected = "1000";
+    //     let event = "LogDeposit";
+    //     let contractBalanceBefore;
+    //     let contractBalanceAfter;
+    //     return web3.eth.getBalancePromise(instance.address)
+    //         .then(_balance  => {
+    //             contractBalanceBefore = _balance;
+    //             return instance.sendRemittance(hashed1, hashed2, {from:owner})
+    //         })
+    //         .then(() => {
+    //             return instance.deposit({from: owner, value: deposit})  
+    //         }).then(txObj => {
+    //             assert.strictEqual(txObj.logs[0].event, event, "a deposit was not logged");
+    //             return web3.eth.getBalancePromise(instance.address)
+    //         }).then(_balance  => {
+    //             contractBalanceAfter = _balance;
+    //             assert.strictEqual(_balance.toString(10), expected, "Splitter balance was not increased by 10 wei");
+    //         })
+    // });
+
+    // it("should allow Carol to unlock the funds by validating her passwords", async() => {
+    //     let hashed1 = 0xbf99d12625bf06ca9eb22dcd41ab8f3e00814126e293aed5c6365f5752512d36;
+    //     let hashed2 = 0x754b1823247e990c20230ca0d8806c29508cc9244c059c09a1869b56ab64dacd;
+    //     let result;
+    //     let deposit = 1000;
+    //     await instance.setPasswords(hashed1, hashed2, {from:owner});
+    //     await instance.deposit({from: owner, value: deposit});
+    //     await instance.validatePasswords(hashed1, hashed2, {from: Carol});
+    //     result = await instance.fundsUnlocked();    
+    //     assert.strictEqual(result, true, "Carol was not able to validate her passwords");
+    // })
+
+    // it("should confirm that carol can withdraw her funds", async() => {
+    //     let startBalance;
+    //     let payout;
+    //     let gasPrice;
+    //     let gasUsed;
+    //     let txFee;
+    //     let endBalance;
+    //     let hashed1 = 0xbf99d12625bf06ca9eb22dcd41ab8f3e00814126e293aed5c6365f5752512d36;
+    //     let hashed2 = 0x754b1823247e990c20230ca0d8806c29508cc9244c059c09a1869b56ab64dacd;
+    //     let deposit = 1000;
+    //     startBalance = await web3.eth.getBalancePromise(Carol);
+    //     await instance.setPasswords(hashed1, hashed2, {from:owner});
+    //     await instance.deposit({from: owner, value: deposit});
+    //     await instance.validatePasswords(hashed1, hashed2, {from: Carol}); 
+    //     txObj = await instance.withdrawFunds({from: Carol});
+    //     gasUsed = txObj.receipt.gasUsed;
+    //     tx = await web3.eth.getTransactionPromise(txObj.tx);
+    //     gasPrice = tx.gasPrice;
+    //     txFee = gasPrice.times(gasUsed);
+    //     endBalance = await web3.eth.getBalancePromise(Carol)
+    //     assert.strictEqual(startBalance.plus(deposit).minus(txFee).toString(10), endBalance.toString(10), "Carol didn't get her ether")  
+    // })
 })
+//0x1b2fd6682ba83a0c9134a85393b2204a76872a8a5f6fb59bfafeacf87fa0930f remix
+//0x8d212deb7e8d8babaa2e984a112fdce282a2689cff0a6d2de36b997545d88b61 tests
